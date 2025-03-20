67a184de542ee21e1b502a2715c0142f742049fb
esaie2 <isharaesaie2003@gmail.com>
2025-03-20 01:08:18 -0700

    Update group of animals.cpp
    Explanation in Simple Terms:
    
    1. Ask for the Number of Animals:
    
    The program first asks the user how many domestic animals (N) and wild animals (X) they want to enter.
    
    2. Store Animal Names:
    
    Two arrays (domesticAnimals and wildAnimals) are created to store the names of the animals. The size of each array depends on the values of N and X.
    
    3. Input Animal Names:
    
    The program uses a loop to ask the user to enter the names of the domestic animals one by one.
    
    Then, it uses another loop to ask for the names of the wild animals.
    
    4. Print All Animals:
    
    After collecting all the names, the program prints all the animals. It first prints the domestic animals and then the wild animals.

--- group of animals.cpp
+++ group of animals.cpp
@@ -6,31 +6,33 @@ using namespace std;
 int main() {
     int N, X;
 
-    // Input the number of domestic and wild animals
+    // here we insert  the number of domestic and wild animals
     cout << "Enter the number of domestic animals (N): ";
     cin >> N;
     cout << "Enter the number of wild animals (X): ";
     cin >> X;
 
-    // Vectors to store the names of domestic and wild animals
+    // the Vectors to store the names of domestic and wild animals ,
+    // is like a flexible and smart container that will hold the domesticAnimals and wildAnimals for us,
+    // It grows or shrinks automatically as you add or remove words, 
     vector<string> domesticAnimals(N);
     vector<string> wildAnimals(X);
 
-    // Input domestic animals
+    // let's now enter the  domestic animals
     cout << "\nEnter the names of " << N << " domestic animals:\n";
     for (int i = 0; i < N; ++i) {
         cout << "  Domestic animal " << i + 1 << ": ";
         cin >> domesticAnimals[i];
     }
 
-    // Input wild animals
+    // the the  wild animals
     cout << "\nEnter the names of " << X << " wild animals:\n";
     for (int i = 0; i < X; ++i) {
         cout << "  Wild animal " << i + 1 << ": ";
         cin >> wildAnimals[i];
     }
 
-    // Print all animals (domestic + wild)
+    // we now print  all animals (domestic + wild)
     cout << "\nAll " << N + X << " animals:\n";
     for (int i = 0; i < N; ++i) {
         cout << "  Domestic animal " << i + 1 << ": " << domesticAnimals[i] << endl;
