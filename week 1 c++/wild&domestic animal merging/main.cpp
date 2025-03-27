a764bf9e4fb9c4cbac26fa91a05678067ea48167
khalidAbdalla <khalidxusen88@icloud.com>
2025-03-27 11:54:31 +0000

    last Update main.cpp

--- week 1 c++/wild&domestic animal merging/main.cpp
+++ week 1 c++/wild&domestic animal merging/main.cpp
@@ -1,60 +1,60 @@
 #include <iostream>
-
 using namespace std;
 
 int main()
 {
-/*
--- this program is about a user entering both domestic and wild animals differently and merging them together in a new array
-*/
-     // Accept a group of domestic and wild animals
+    /*
+    -- This program allows the user to enter domestic and wild animals separately
+    -- and then combines them into a single merged list.
+    */
+
     int N, X;
 
     cout << "Enter the number of domestic animals: ";
     cin >> N;
     string domesticAnimals[N];
 
-
     for (int i = 0; i < N; i++) {
         cout << "Domestic animal " << (i + 1) << ": ";
         cin >> domesticAnimals[i];
     }
 
-
     cout << "\nEnter the number of wild animals: ";
     cin >> X;
     string wildAnimals[X];
-
 
     for (int i = 0; i < X; i++) {
         cout << "Wild animal " << (i + 1) << ": ";
         cin >> wildAnimals[i];
     }
 
-/*
-creating a merged array for both our wild and domestic animals
-*/
-    int totalSize= N+X;
+    /*
+    -- Creating a merged array for both wild and domestic animals
+    */
+    int totalSize = N + X;
     string AllAnimals[totalSize];
 
-    // for copying domestic animals into our merged array
-  for (int i=0; i<N; i++)
-    {
-    AllAnimals[i]=domesticAnimals[i];
-}
-// for copying wild animals into our merged array
-  for (int i=0; i<X; i++)
-    {
-    AllAnimals[N+i]=wildAnimals[i];
-}
-    cout << "\nAll animals entered (Domestic + Wild):\n";
+    // Copy domestic animals
     for (int i = 0; i < N; i++) {
-        cout << domesticAnimals[i] << endl;
+        AllAnimals[i] = domesticAnimals[i];
     }
+
+    // Copy wild animals
     for (int i = 0; i < X; i++) {
-        cout << wildAnimals[i] << endl;
+        AllAnimals[N + i] = wildAnimals[i];
     }
+
+    /*
+    -- Displaying the merged array
+    */
+    cout << "\nAll animals entered (Domestic + Wild):\n";
+    cout << "-------------------------------------\n";
+
+    for (int i = 0; i < totalSize; i++) {
+        cout << i + 1 << ". " << AllAnimals[i] << endl;
+    }
+
+    cout << "\nThank you for using the animal list manager!\n";
 
     return 0;
 }
-
